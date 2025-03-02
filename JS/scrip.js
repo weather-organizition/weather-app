a50d4d725c4ee28e79a6b42a9c9e6541fbdce845
Shukri Mohamed <Shukriiman83@gmail.com>
2025-03-02 16:24:43 +0800

    fix minor error

--- JS/scrip.js
+++ JS/scrip.js
@@ -285,7 +285,7 @@ function createAlertElement(alert) {
       const tipsListElement = safetyTipElement.querySelector(".tips-list");
       const safetyTips = getSafetyTipsForAlert(alert);
 
-      safetyTips.forEach((tip) => {git 
+      safetyTips.forEach((tip) => {
         const listItem = document.createElement("li");
         listItem.classList.add('safety-tips-list')
         listItem.innerHTML = `<strong>${tip}</strong>`;
